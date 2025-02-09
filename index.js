b41db04dc3c7cd452de0da5a44cac877d61a3c2d
Ivy Xu <i8xu@uwaterloo.ca>
2025-02-08 20:45:23 -0500

    next section buttons

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function scrollToNextSection() {
+function scrollToExperience() {
     const nextSection = document.querySelector('#experience');
+    if (nextSection) {
+        nextSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function scrollToProjects() {
+    const nextSection = document.querySelector('#projects');
     if (nextSection) {
         nextSection.scrollIntoView({ behavior: 'smooth' });
     }
